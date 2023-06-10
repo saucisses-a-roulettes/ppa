3e461c4c74d4be6aafa6c55385abc4f7fc9ac81d
Saucisse à roulettes <gaelmonach123@live.fr>
2023-06-10 23:11:12 +0200

    [PPA-2] InMemoryRepository Generation System (#1)
    * impl(InMemoryRepository): metaclass (WIP)
    
    * impl(InMemoryRepository): metaclass's _make_retrieve_method
    
    * impl(InMemoryRepository): metaclass's _make_update_method
    
    * impl(InMemoryRepository): decorator and tests
    
    * impl(InMemoryRepository): decorator and tests
    
    * impl(InMemoryRepository): decorator and tests
    
    * fix(InMemoryRepository): types and syntax
    
    * add(CI): ci
    
    * version(toml): 0.2.0

---
+++ src/in_memory.py
@@ -0,0 +1,42 @@
+from typing import Any, Type
+
+
+def in_memory_behavior(cls) -> Type:
+    class InMemoryRepository:
+        find_by_fields: list[str] = ["id"]
+        entity_already_exists_exception: Type[Exception] = ValueError
+        entity_not_found_exception: Type[Exception] = ValueError
+
+        def __init__(self) -> None:
+            self._store: dict[str, Any] = {}
+            for key in self.find_by_fields:
+                setattr(self, f"find_by_{key}", self._make_find_by_method(key))
+
+        def add(self, entity: Any) -> Any:
+            if entity.id in self._store:
+                raise self.entity_already_exists_exception()
+            self._store[entity.id] = entity
+
+        def retrieve(self, id_: Any) -> Any:
+            try:
+                return self._store[id_]
+            except KeyError as err:
+                raise self.entity_not_found_exception() from err
+
+        def update(self, entity: Any) -> None:
+            if entity.id not in self._store:
+                raise self.entity_not_found_exception()
+            self._store[entity.id] = entity
+
+        def delete(self, id_: Any) -> None:
+            if id_ not in self._store:
+                raise self.entity_not_found_exception(id_)
+            del self._store[id_]
+
+        def _make_find_by_method(self, field_name: str) -> Any:
+            def find_by_method(value: Any) -> list[Any]:
+                return [entity for id_, entity in self._store.items() if getattr(entity, field_name) == value]
+
+            return find_by_method
+
+    return InMemoryRepository
