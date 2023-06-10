4d71a8c4ed76186861bd6411a4a36f56ea54abfc
Saucisse à roulettes <gaelmonach123@live.fr>
2023-06-10 22:55:49 +0200

    impl(InMemoryRepository): decorator and tests

--- src/in_memory.py
+++ src/in_memory.py
@@ -1,114 +1,46 @@
-from __future__ import annotations
-import dataclasses
-from abc import abstractmethod, ABC
-from dataclasses import dataclass
-from typing import Annotated, Generic, TypeVar, Protocol, ClassVar, Dict, Any, Type, Self
-
-from pydantic import BaseModel
-
-_TId = TypeVar("_TId")
+from typing import Any, Type
 
 
-class _Entity(Protocol):
+def in_memory_behavior(cls):
+    class InMemoryRepository(cls):
+        find_by_fields: list[str] = ["id"]
+        entity_already_exists_exception: Type[Exception] = ValueError
+        entity_not_found_exception: Type[Exception] = ValueError
 
-    @property
-    @abstractmethod
-    def id(self) -> _TId:
-        pass
+        def __init__(self):
+            self._store = {}
+            for key in self.find_by_fields:
+                setattr(self, f"find_by_{key}", self._make_find_by_method(key))
 
-
-_TEntity = TypeVar("_TEntity")
-
-
-
-class InMemoryRepositoryMetaclass(type):
-
-    # def __init__(self, model: Type[BaseModel]) -> None:
-    #     self._model = model
-    #     self._store: dict[_TId, _TEntity] = {}
-    #     self._create_find_by_methods()
-
-    def __new__(cls: Type[InMemoryRepositoryMetaclass], name: str, bases: tuple, class_dict: dict[str, Any]) -> InMemoryRepositoryMetaclass:
-        result = super().__new__(cls, name, bases, class_dict)
-        if fields := class_dict.get("find_by_fields"):
-            for key in fields:
-                setattr(result, f'find_by_{key}', cls._make_find_by_method(key))
-
-        if e := class_dict.get("entity_already_exists_exception"):
-            setattr(result, 'add', cls._make_add_method(e))
-        if e := class_dict.get("entity_not_found_exception"):
-            setattr(result, 'retrieve', cls._make_retrieve_method(e))
-            setattr(result, 'update', cls._make_update_method(e))
-
-        return result
-
-    @staticmethod
-    def _make_find_by_method(field_name: str) -> Any:
-        def find_by_method(self, value: Any) -> list[Any]:
-            return [entity for id_, entity in self._store.items() if getattr(entity, field_name) == value]
-
-        return find_by_method
-
-    @staticmethod
-    def _make_add_method(entity_already_exists_exception: Type[Exception]) -> Any:
-        def add_method(self, entity: Any) -> Any:
+        def add(self, entity: Any) -> Any:
             if entity.id in self._store:
-                raise entity_already_exists_exception()
+                raise self.entity_already_exists_exception()
             self._store[entity.id] = entity
 
-        return add_method
-
-    @staticmethod
-    def _make_retrieve_method(entity_not_found_exception: Type[Exception]) -> Any:
-        def retrieve_method(self, id_: Any) -> Any:
+        def retrieve(self, id_: Any) -> Any:
             try:
                 return self._store[id_]
             except KeyError as err:
-                raise entity_not_found_exception() from err
-        return retrieve_method
+                raise self.entity_not_found_exception() from err
 
-    @staticmethod
-    def _make_update_method(entity_not_found_exception: Type[Exception]) -> Any:
-        def update_method(self, entity: Any) -> None:
+        def update(self, entity: Any) -> None:
             if entity.id not in self._store:
-                raise entity_not_found_exception()
+                raise self.entity_not_found_exception()
             self._store[entity.id] = entity
 
-        return update_method
+        def delete(self, id_: Any) -> None:
+            if id_ not in self._store:
+                raise self.entity_not_found_exception(id_)
+            del self._store[id_]
 
+        def _make_find_by_method(self, field_name: str) -> Any:
+            def find_by_method(value: Any) -> list[Any]:
+                return [
+                    entity
+                    for id_, entity in self._store.items()
+                    if getattr(entity, field_name) == value
+                ]
 
+            return find_by_method
 
-
-
-    # def delete(self, id_: Id) -> None:
-    #     if id_ not in self._store:
-    #         raise CannotRetrieveEntity(id_)
-    #     del self._store[id_]
-    #
-    # def all(self) -> List[TEntity]:
-    #     return list(self._store.values())
-
-
-class InMemoryRepository(metaclass=InMemoryRepositoryMetaclass):
-    find_by_fields: list[str] = ["id"]
-    entity_already_exists_exception: Type[Exception] = ValueError
-    entity_not_found_exception: Type[Exception] = ValueError
-
-    def __init__(self) -> None:
-        self._test = "test"
-        self._store = {}
-
-
-class Test:
-
-    @property
-    def id(self) -> int:
-        return 1
-
-
-rep = InMemoryRepository()
-
-rep.add(Test())
-print(rep.find_by_id(1)[0].id)
-rep.update(Test())
-print(rep.retrieve(1).id)
+    return InMemoryRepository
---
+++ src/test/test_in_memory.py
@@ -0,0 +1,37 @@
+import pytest
+
+from src.in_memory import in_memory_behavior
+
+
+@pytest.fixture
+def User():
+    class User:
+        def __init__(self, id: int, name: str):
+            self.id = id
+            self.name = name
+
+    return User
+
+@pytest.fixture
+def UserRepository(User):
+    @in_memory_behavior
+    class UserRepository:
+        pass
+
+    return UserRepository()
+
+@pytest.mark.parametrize("entity_id, entity_name", [(1, "John"), (2, "Alice"), (3, "Bob")])
+def test_in_memory_behavior(UserRepository, User, entity_id, entity_name):
+    user = User(id=entity_id, name=entity_name)
+
+    UserRepository.add(user)
+    retrieved_user = UserRepository.retrieve(entity_id)
+    assert retrieved_user == user
+
+    updated_user = User(id=entity_id, name="Updated Name")
+    UserRepository.update(updated_user)
+    assert UserRepository.retrieve(entity_id) == updated_user
+
+    UserRepository.delete(entity_id)
+    with pytest.raises(ValueError):
+        UserRepository.retrieve(entity_id)
